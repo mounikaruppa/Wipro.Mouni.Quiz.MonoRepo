ec4155aeb48d7b959f0aef0ad8758d9ea13b8f50
mounikaruppa <mounikaruppa1981@gmail.com>
2025-08-10 17:44:58 +0530

    Update QuestionService.java

--- quiz/src/main/java/com/example/quiz/service/QuestionService.java
+++ quiz/src/main/java/com/example/quiz/service/QuestionService.java
@@ -1,4 +1,4 @@
-package com.example.quiz.service;
+package com.example.wipro.mouni.quiz.service;
 
 import com.example.quiz.entity.Question;
 
@@ -20,3 +20,4 @@ public interface QuestionService {
 
 	
 }
+
