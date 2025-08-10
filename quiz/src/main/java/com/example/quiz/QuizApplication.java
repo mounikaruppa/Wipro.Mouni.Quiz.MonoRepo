384181349a3118b8a994692b3212afc496a30982
mounikaruppa <mounikaruppa1981@gmail.com>
2025-08-10 17:46:46 +0530

    Update QuizApplication.java

--- quiz/src/main/java/com/example/quiz/QuizApplication.java
+++ quiz/src/main/java/com/example/quiz/QuizApplication.java
@@ -1,4 +1,4 @@
-package com.example.quiz;
+package com.example.wipro.mouni.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
